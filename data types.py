6e1dfc14bf226dbf78c72ad17f25d345895737c8
ayan123056 <jasirafowaz@yahoo.com>
2024-10-23 21:06:54 +0400

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,11 @@
+name="penguin"
+age = 15
+student=True
+weight=38.5
+print("name: ",name)
+print ("data type of name", type(name))
+print("age: ",age)
+print("data type of name", type(age))
+age = float(age)
+print(age) 
+print("data type of age after converting", type(age)) 
