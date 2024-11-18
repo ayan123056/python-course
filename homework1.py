dcfd27291907ab8353b25b9e6c43ef893159c533
ayan123056 <jasirafowaz@yahoo.com>
2024-11-18 20:04:07 +0400

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,11 @@
+x=input("Enter value of x:")
+y=input("Enter value of y:")
+z=input("Enter value of z")
+temp=x
+x=y
+y=temp
+z=y
+z=temp
+print("value of x after swapping",x)
+print("value of y after swapping",y)
+print("Value of z after swapping",z)
